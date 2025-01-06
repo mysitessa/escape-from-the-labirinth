37b62602da8883ec2e481a019fa2ffee31b9de53
Angelinacekoeva <angelinacekoeva9@gmail.com>
2025-01-06 22:44:37 +0300

    main file for code

---
+++ level_style.py
@@ -0,0 +1,49 @@
+from settings import *
+
+WHITE = (255, 255, 255)
+
+get_config()
+settings = start_app()
+screen = settings[0]
+FPS = settings[1]
+
+
+class Board:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[0] * width for _ in range(height)]
+        # значения по умолчанию
+        self.left = 10
+        self.top = 10
+        self.cell_size = 60
+
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    def render(self, screen):
+        for y in range(self.height):
+            for x in range(self.width):
+                pygame.draw.rect(screen, WHITE,
+                                 (self.left + self.cell_size * x,
+                                  self.top + self.cell_size * y,
+                                  self.cell_size, self.cell_size), 1)
+
+
+def start_game():
+    clock = pygame.time.Clock()
+    b = Board(10, 10)
+    running = True
+    while running:
+        screen.fill((0, 0, 0))
+        for event in pygame.event.get():
+            # при закрытии окна
+            if event.type == pygame.QUIT:
+                running = False
+        b.render(screen)
+
+        clock.tick(FPS)
+        pygame.display.flip()
+    pygame.quit()
