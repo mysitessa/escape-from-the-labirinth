94fb0ea548f12843fba420df41a5e76fc0ad9aca
Angelinacekoeva <angelinacekoeva9@gmail.com>
2025-01-25 15:52:25 +0300

    NONE

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-#from login_window import *
-from settings import start_app
-from start_window import start_game
+from login_window import *
+
 
 def main():
     start_game()
+
 
 if __name__ == '__main__':
     main()
